535a8f8006d90a3f69dcd62086a36ff69e02b832
eatingC0933 <yiting.chang@iths.se>
2025-01-26 11:34:45 +0100

    Added conflictingMethod in develop branch

--- src/main/java/se/yitingchang/Main.java
+++ src/main/java/se/yitingchang/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public void conflictingMethod() {
+        System.out.println("Conflicting method in develop branch");
+
+    }
 }
