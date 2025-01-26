bf54a0a15fb11e3a8117a724e5836686e20e9b29
eatingC0933 <yiting.chang@iths.se>
2025-01-26 12:17:06 +0100

    Added featureMethod in conflicting-feature branch

--- src/main/java/se/yitingchang/Main.java
+++ src/main/java/se/yitingchang/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public void featureMethod() {
+        System.out.println("Feature method in conflicting-feature branch");
+    }
 }
